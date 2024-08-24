8d2a629f53cfc49e12fb567e557253ff8210542f
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-08-24 14:11:09 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,24 @@
-// Import necessary packages
 import express from "express";
 import cors from "cors";
-import { Client } from "pg"; // PostgreSQL client
+import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import 'dotenv/config';
 
-// app config
+// App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-// PostgreSQL connection setup
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT, // default PostgreSQL port is 5432
-});
-
-client.connect()
-  .then(() => console.log('Connected to PostgreSQL database'))
-  .catch(err => console.error('Connection error', err.stack));
-
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// api endpoints
+// DB connection
+connectDB();
+
+// API endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static('uploads'));
 app.use("/api/user", userRouter);
@@ -37,12 +26,10 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working");
+    res.send("API Working");
 });
 
+// Start server
 app.listen(port, () => {
-  console.log(`Server Started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`);
 });
-
-// Export client for use in other modules
-export { client };
