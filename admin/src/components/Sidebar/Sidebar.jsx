2058b995ac834666ed614daa234cd4bd7800001a
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-09-14 23:59:23 +0530

    Update Admin

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Sidebar.css'
-import { assets } from '../../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import './Sidebar.css';
+import { assets } from '../../assets/assets';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className="sidebar-options">
-            <NavLink to='/add' className="sidebar-option">
-                <img className='addd' src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
-            <NavLink to='/list' className="sidebar-option">
-                <img className='listt' src={assets.order_icon} alt="" />
-                <p>List Items</p>
-                </NavLink>
-            <NavLink to='/orders' className="sidebar-option">
-                <img className='orderr' src={assets.order_icon} alt="" />
-                <p>Orders</p>
-                </NavLink>
-        </div>
+      <div className="sidebar-options">
+        <NavLink to='/add' className="sidebar-option">
+          <img className='icon' src={assets.add_icon} alt="Add" />
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
+          <img className='icon' src={assets.list} alt="List" />
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
+          <img className='icon' src={assets.order_icon} alt="Orders" />
+          <p>Orders</p>
+        </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
