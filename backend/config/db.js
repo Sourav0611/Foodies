75bda7b1d0f471fe8fd2fdf58b7013c031fbb1bc
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-09-15 10:04:46 +0530

    Fix info

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import dotenv from "dotenv"
+
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://jaiswalsourav5:123456789Sj159@cluster0.vzhcw.mongodb.net/foodies').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DataBase Connected"));
 }
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,7 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
 import Stripe from "stripe"
 
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
