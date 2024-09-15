6924b8beadb10a6a52dfefec16b9b17817a64bef
Sourav Jaiswal <jaiswalsourav5@gmail.com>
2024-09-15 08:58:14 +0530

    Update backend modules

---
+++ backend/node_modules/react-native-razorpay/ios/RazorpayCheckout.h
@@ -0,0 +1,13 @@
+//
+//  RazorpayCheckout.h
+//  RazorpayCheckout
+//
+//  Created by Akshay Bhalotia on 31/08/16.
+//  Copyright © 2016 Razorpay. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+
+@interface RNRazorpayCheckout : NSObject <RCTBridgeModule>
+
+@end
